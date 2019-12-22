a3a594cfab8273b6ec1087e1dd689df3326ca4fd
김성진 <l0rq2@naver.com>
2019-12-23 03:11:32 +0900

    Create make_haksa.py

---
+++ make_haksa.py
@@ -0,0 +1,21 @@
+import requests, re, json, sys
+import datetime
+now=datetime.datetime.now()
+URL="http://mmhs.hs.kr/66333/subMenu.do?srhSchdulYear="+str(now.year)+"&srhSchdulMonth="+str(now.month)
+
+response = requests.get(URL).text
+
+data = response[response.find("<tbody>"):response.find("</tbody>")]
+
+data= re.sub('<.+?>', '', data, 0, re.I|re.S)
+data=re.sub('&nbsp;|\t|\r|\n', '', data)
+data_split=data.split()
+day=1;
+for i in range(0, len(data_split)):
+    if(str(data_split[i])==str(day)):
+        data_split[i]="\n"+data_split[i]
+        day+=1
+data=" ".join(data_split)
+print(data)
+f = open("haksa/haksa", 'w')
+f.write(data)
