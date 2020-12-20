c8ebabca89693eb22e2968e1320262f1f1a172a9
김성진 <l0rq2@naver.com>
2020-12-20 13:34:11 +0900

    Update make_json.py

--- make_json.py
+++ make_json.py
@@ -26,11 +26,14 @@ for dat in data:
         date=date[0]
     menu = menu.split("<br />")
     file_json.update({date : menu})
+    
 ##Json 생성
 for i in range(1, len(file_json)+1):
     if(len(file_json[str(i)])==1):
         file_json[str(i)]=u"오늘의 급식정보가 존재하지 않습니다."
-with open('menu/menu.json', 'w', encoding='utf-8') as outfile:
+        
+with open('/workspace/MMBOT/menu/menu.json', 'w', encoding='utf-8') as outfile:
        json.dump(file_json, outfile, sort_keys = False, indent=4, ensure_ascii=0)
+#------------------------------------------------------------------------------------------------calender
 
     
